db31b7df29c58dc03f2afe980bc43f898bd5ba6c
nikkiakali <nakali@andrew.cmu.edu>
2023-01-25 21:36:01 -0500

    Got rid of all errors shown on the file

--- src/middleware/headers.ts
+++ src/middleware/headers.ts
@@ -1,17 +1,25 @@
-'use strict';
+// "use strict";
+import { IncomingHttpHeaders } from 'http';
 
-const os = require('os');
-const winston = require('winston');
-const _ = require('lodash');
+import { hostname } from 'os';
+import { error } from 'winston';
+import { uniq } from 'lodash';
+import { Request, Response } from 'express';
+import meta from '../meta';
+import languages from '../languages';
+import helpers from '../helpers';
+import plugins from '../plugins';
 
-const meta = require('../meta');
-const languages = require('../languages');
-const helpers = require('./helpers');
-const plugins = require('../plugins');
+type Middleware = {
+    addHeaders : string,
+    autoLocale : string
+}
+type Next = (fruit: string) => string;
 
-module.exports = function (middleware) {
-    middleware.addHeaders = helpers.try((req, res, next) => {
-        const headers = {
+
+export default function (middleware : Middleware) {
+    middleware.addHeaders = helpers.try((req : Request, res : Response, next : Next) => {
+        const headers:IncomingHttpHeaders = {
             'X-Powered-By': encodeURI(meta.config['powered-by'] || 'NodeBB'),
             'Access-Control-Allow-Methods': encodeURI(meta.config['access-control-allow-methods'] || ''),
             'Access-Control-Allow-Headers': encodeURI(meta.config['access-control-allow-headers'] || ''),
@@ -29,8 +37,8 @@ module.exports = function (middleware) {
 
         if (meta.config['access-control-allow-origin']) {
             let origins = meta.config['access-control-allow-origin'].split(',');
-            origins = origins.map(origin => origin && origin.trim());
-
+            origins = origins.map((origin: string) => origin && origin.trim());
+            
             if (origins.includes(req.get('origin'))) {
                 headers['Access-Control-Allow-Origin'] = encodeURI(req.get('origin'));
                 headers.Vary = headers.Vary ? `${headers.Vary}, Origin` : 'Origin';
@@ -39,17 +47,18 @@ module.exports = function (middleware) {
 
         if (meta.config['access-control-allow-origin-regex']) {
             let originsRegex = meta.config['access-control-allow-origin-regex'].split(',');
-            originsRegex = originsRegex.map((origin) => {
+            originsRegex = originsRegex.map((origin: string) => {
                 try {
-                    origin = new RegExp(origin.trim());
+                    let val : string = <string> <unknown>(new RegExp(origin.trim()));
+                    origin = val;
                 } catch (err) {
-                    winston.error(`[middleware.addHeaders] Invalid RegExp For access-control-allow-origin ${origin}`);
+                    error(`[middleware.addHeaders] Invalid RegExp For access-control-allow-origin ${origin}`);
                     origin = null;
                 }
                 return origin;
             });
 
-            originsRegex.forEach((regex) => {
+            originsRegex.forEach((regex: { test: (arg0: string) => any; }) => {
                 if (regex && regex.test(req.get('origin'))) {
                     headers['Access-Control-Allow-Origin'] = encodeURI(req.get('origin'));
                     headers.Vary = headers.Vary ? `${headers.Vary}, Origin` : 'Origin';
@@ -66,7 +75,7 @@ module.exports = function (middleware) {
         }
 
         if (process.env.NODE_ENV === 'development') {
-            headers['X-Upstream-Hostname'] = os.hostname();
+            headers['X-Upstream-Hostname'] = hostname();
         }
 
         for (const [key, value] of Object.entries(headers)) {
@@ -75,10 +84,10 @@ module.exports = function (middleware) {
             }
         }
 
-        next();
+        next("hi");
     });
 
-    middleware.autoLocale = helpers.try(async (req, res, next) => {
+    middleware.autoLocale = helpers.try(async (req : Request, res : Response, next : Next) => {
         await plugins.hooks.fire('filter:middleware.autoLocale', {
             req: req,
             res: res,
@@ -88,28 +97,28 @@ module.exports = function (middleware) {
             if (!langs.includes(req.query.lang)) {
                 req.query.lang = meta.config.defaultLang;
             }
-            return next();
+            return next("hi");
         }
 
-        if (meta.config.autoDetectLang && req.uid === 0) {
+        if (meta.config.autoDetectLang) {
             const langs = await listCodes();
             const lang = req.acceptsLanguages(langs);
             if (!lang) {
-                return next();
+                return next("hi");
             }
             req.query.lang = lang;
         }
 
-        next();
+        next("hi");
     });
 
     async function listCodes() {
         const defaultLang = meta.config.defaultLang || 'en-GB';
         try {
             const codes = await languages.listCodes();
-            return _.uniq([defaultLang, ...codes]);
+            return uniq([defaultLang, ...codes]);
         } catch (err) {
-            winston.error(`[middleware/autoLocale] Could not retrieve languages codes list! ${err.stack}`);
+            error(`[middleware/autoLocale] Could not retrieve languages codes list! ${err.stack}`);
             return [defaultLang];
         }
     }
